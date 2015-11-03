cbcdc574958f616084463405e65f0b9a68c92f4e
Ramaniuk <kanumowa@gmail.com>
2015-11-03 15:57:46 +0000

    Add jquery selector

--- js/main.js
+++ js/main.js
@@ -120,6 +120,8 @@ function addInput(argument) {
 var summvalue = 0;
 var tableHeader1 = "<thead><tr><th>Время</th><th>Сумма</th><th>Описание</th><th>Тип</th></tr></thead>";
 
+$(".daterange--double").click(function () {alert(dd.start_date)});
+
 function refresh() {
     var cal_start=dd.start_date;
     var cal_end=dd.end_date;
